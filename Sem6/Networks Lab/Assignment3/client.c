8fdc28af73d3973a507accd140739a4af4cad99c
Rashi Singh <rashi170101052@iitg.ac.in>
2020-03-11 01:58:32 +0530

    Phase 2 : UDP completed

--- Sem6/Networks Lab/Assignment3/client.c
+++ Sem6/Networks Lab/Assignment3/client.c
@@ -8,6 +8,7 @@
 
 #define PORT 9002 
 typedef struct sockaddr_in socketAddress;
+typedef struct sockaddr* socketAddressPtr;
 
 void printError(char* msg)
 {
@@ -17,26 +18,63 @@ void printError(char* msg)
 
 int main(int argc, char const *argv[]) 
 { 
-	int client_socket = -1; 
-	if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) printError("Client Socket not created");
-    else printf("Client socket created successfully\n");
+    //**************UDP port negotiation******************
 
-	socketAddress server_addr; 
-	server_addr.sin_family = AF_INET; 
-	server_addr.sin_port = htons(PORT); 
-	server_addr.sin_addr.s_addr = INADDR_ANY;
+	int client_TCP_socket = -1; 
+	if ((client_TCP_socket = socket(AF_INET, SOCK_STREAM, 0)) < 0) printError("Client TCP socket not created");
+    else printf("Client TCP socket created successfully\n");
 
-	if (connect(client_socket, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) printError("Connection error");
+	socketAddress server_TCP_addr; 
+    memset(&server_TCP_addr, 0, sizeof(server_TCP_addr)); 
+	server_TCP_addr.sin_family = AF_INET; 
+	server_TCP_addr.sin_port = htons(PORT); 
+	server_TCP_addr.sin_addr.s_addr = INADDR_ANY;
+
+	if (connect(client_TCP_socket, (socketAddressPtr)&server_TCP_addr, sizeof(server_TCP_addr)) < 0) printError("Connection error");
     else printf("TCP Connection created successfully\n");
 
     char *request_msg = "UDP port requested"; 
-	char server_response[1024] = {0}; 
+	char UDP_port_str[1024] = {0};  //server response
 
-	send(client_socket , request_msg , strlen(request_msg) , 0 ); 
+	send(client_TCP_socket , request_msg , strlen(request_msg) , 0 ); 
 	printf("UDP port request sent\n"); 
 
     int read_status = -1;
-	read_status = read( client_socket , server_response, 1024); 
-	printf("Server Response : %s\n",server_response ); 
+	read_status = read( client_TCP_socket , UDP_port_str, 1024); 
+	printf("Server UDP port : %s\n\n\n",UDP_port_str ); 
+
+    int UDP_PORT = atoi(UDP_port_str);
+
+    close(client_TCP_socket);
+
+    //**************UDP port negotiation completed******************
+
+    int client_UDP_socket; 
+
+    // Creating socket file descriptor 
+    if ( (client_UDP_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) printError("Client UDP Socket not created");
+    else printf("Client UDP socket created successfully\n");
+
+    //filling server information
+    socketAddress server_UDP_addr; 
+    memset(&server_UDP_addr, 0, sizeof(server_UDP_addr)); 
+    server_UDP_addr.sin_family = AF_INET; 
+    server_UDP_addr.sin_port = htons(UDP_PORT); 
+    server_UDP_addr.sin_addr.s_addr = INADDR_ANY; 
+      
+    int server_UDP_addr_len; 
+    char client_msg[1024];   
+    printf("Please input msg to the server\n");
+    scanf("%s",client_msg);
+
+    sendto(client_UDP_socket, (const char *)client_msg, strlen(client_msg), MSG_CONFIRM, (socketAddressPtr) &server_UDP_addr, sizeof(server_UDP_addr)); 
+    printf("Client message sent.\n"); 
+          
+    char server_response[1024]; 
+    int server_response_len = recvfrom(client_UDP_socket, (char *)server_response, 1024, MSG_WAITALL, (socketAddressPtr) &server_UDP_addr, &server_UDP_addr_len); 
+    server_response[server_response_len] = '\0'; 
+    printf("Server response : %s\n", server_response); 
+  
+    close(client_UDP_socket); 
 	return 0; 
 } 
--- Sem6/Networks Lab/Assignment3/server.c
+++ Sem6/Networks Lab/Assignment3/server.c
@@ -7,6 +7,7 @@
 #include <string.h> 
 #define PORT 9002
 typedef struct sockaddr_in socketAddress;
+typedef struct sockaddr* socketAddressPtr;
 
 void printError(char* msg)
 {
@@ -16,25 +17,28 @@ void printError(char* msg)
 
 int main(int argc, char const *argv[]) 
 { 
+    //**************UDP port negotiation******************
+
 	// Creating socket file descriptor 
-	int server_socket; 
-	if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) printError("Server Socket not created");
+	int server_TCP_socket; 
+	if ((server_TCP_socket = socket(AF_INET, SOCK_STREAM, 0)) == 0) printError("Server Socket not created");
     else printf("Welcome socket created successfully\n");
 
-    socketAddress server_addr; 	
-	server_addr.sin_family = AF_INET; 
-	server_addr.sin_addr.s_addr = INADDR_ANY; 
-	server_addr.sin_port = htons( PORT ); 
+    socketAddress server_TCP_addr; 	
+    memset(&server_TCP_addr, 0, sizeof(server_TCP_addr)); 
+	server_TCP_addr.sin_family = AF_INET; 
+	server_TCP_addr.sin_addr.s_addr = INADDR_ANY; 
+	server_TCP_addr.sin_port = htons( PORT ); 
 	
-	// Forcefully attaching socket to the port 8080 
-	if (bind(server_socket, (struct sockaddr *)&server_addr, sizeof(server_addr))<0) printError("Welcome socket bind failure");
+	// Forcefully attaching socket to the port PORT
+	if (bind(server_TCP_socket, (socketAddressPtr)&server_TCP_addr, sizeof(server_TCP_addr))<0) printError("Welcome socket bind failure");
     else printf("Welcome socket bind success\n");
 
-	if (listen(server_socket, 3) < 0) printError("Listen");
+	if (listen(server_TCP_socket, 3) < 0) printError("Listen");
 
-	int server_addr_len = sizeof(server_addr);  
+	int server_TCP_addr_len = sizeof(server_TCP_addr);  
     int connection_socket;
-	if ((connection_socket = accept(server_socket, (struct sockaddr *)&server_addr, (socklen_t*)&server_addr_len))<0) printError("Accept");
+	if ((connection_socket = accept(server_TCP_socket, (socketAddressPtr)&server_TCP_addr, (socklen_t*)&server_TCP_addr_len))<0) printError("Accept");
     else printf("Connection Socket created successfully\n");
 
     char client_request[1024] = {0}; 
@@ -51,6 +55,50 @@ int main(int argc, char const *argv[])
     sprintf(UDP_PORT_str,"%d",UDP_PORT);
     
 	send(connection_socket , UDP_PORT_str , strlen(UDP_PORT_str) , 0 ); 
-	printf("UDP PORT sent\n"); 
+	printf("UDP PORT sent\n\n\n"); 
+
+    close(connection_socket);
+
+    //**************UDP port negotiation completed******************
+
+    int server_UDP_socket; 
+
+    // Creating socket file descriptor 
+    if ( (server_UDP_socket = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
+        perror("socket creation failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+      
+    socketAddress server_UDP_addr;       
+    memset(&server_UDP_addr, 0, sizeof(server_UDP_addr)); 
+      
+    // Filling server information 
+    server_UDP_addr.sin_family = AF_INET; // IPv4 
+    server_UDP_addr.sin_port = htons(UDP_PORT); 
+    server_UDP_addr.sin_addr.s_addr = INADDR_ANY; 
+      
+    // Bind the socket with the server address 
+    if ( bind(server_UDP_socket, (const socketAddressPtr)&server_UDP_addr, sizeof(server_UDP_addr)) < 0 ) printError("Server UDP socket bind failure");
+    else printf("Server UDP socket bind success\n");
+      
+    socketAddress client_UDP_addr;
+    memset(&client_UDP_addr, 0, sizeof(client_UDP_addr)); 
+
+    int client_UDP_addr_len = sizeof(client_UDP_addr);  
+  
+    char client_msg[1024]; 
+    int client_msg_len;   
+
+    client_msg_len = recvfrom(server_UDP_socket, (char *)client_msg, 1024, MSG_WAITALL, (socketAddressPtr) &client_UDP_addr, &client_UDP_addr_len); 
+    client_msg[client_msg_len] = '\0'; 
+    printf("Client UDP msg : %s\n", client_msg); 
+
+    char server_reply[1024];
+    printf("Please input response to the client\n");
+    scanf("%s",server_reply);
+
+    sendto(server_UDP_socket, (const char *)server_reply, strlen(server_reply), MSG_CONFIRM, (socketAddressPtr) &client_UDP_addr, client_UDP_addr_len); 
+    printf("Server reply sent.\n");
+    close(server_UDP_socket);
 	return 0; 
 } 
