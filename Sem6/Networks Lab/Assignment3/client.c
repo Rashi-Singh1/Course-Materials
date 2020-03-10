6e256e9a4739f983421156c204c01b564229c358
Rashi Singh <rashi170101052@iitg.ac.in>
2020-03-11 00:08:24 +0530

    Read error persists

--- Sem6/Networks Lab/Assignment3/client.c
+++ Sem6/Networks Lab/Assignment3/client.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #include <string.h>
 #define S_PORT 9002
 typedef struct sockaddr_in socketAddress;
@@ -15,9 +16,9 @@ void printError(const char* msg)
 int main(int argc, char const *argv[])
 {
     //creation of client socket
-    int client_socket = 0;
+    int client_socket = -1;
     client_socket = socket(AF_INET,SOCK_STREAM,0);
-    if(client_socket < 0) printError("Client Socket not created\n");
+    if(client_socket < 0) printError("Client Socket not created");
     else printf("Client socket created successfully\n");
 
     socketAddress server_addr;
@@ -25,13 +26,22 @@ int main(int argc, char const *argv[])
     server_addr.sin_port = htons(S_PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
 
-    int connection_status = connect(client_socket,(struct sockaddr* )&server_addr,sizeof(server_addr));
-    if(connection_status < 0) printError("Connection error\n");
+    int connection_status = -1;
+    connection_status = connect(client_socket,(struct sockaddr* )&server_addr,sizeof(server_addr));
+    if(connection_status < 0) printError("Connection error");
     else printf("TCP Connection created successfully\n");
 
     char* request_msg = "UDP port requested";
-    send(client_socket,request_msg,strlen(request_msg),0);
+    int send_status;
+    if(send_status = write(client_socket,request_msg,strlen(request_msg)) < 0) printError("Client request send error");
+    else printf("UDP port request sent\n");
 
+    char UDP_PORT_str[1024] = {0};
+    int valread;
+    if(valread = read(client_socket,UDP_PORT_str,1024) < 0) printError("Server response read error");
+    else printf("Received port number : %s\n",UDP_PORT_str);
+    // recv(client_socket,UDP_PORT_str,sizeof(UDP_PORT_str),0);
+    // printf("Received port number : %s\n",UDP_PORT_str);
     // close(client_socket);
     return 0;
 }
--- Sem6/Networks Lab/Assignment3/server.c
+++ Sem6/Networks Lab/Assignment3/server.c
@@ -2,6 +2,7 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netinet/in.h>
+#include <unistd.h>
 #include <string.h>
 #define S_PORT 9002
 typedef struct sockaddr_in socketAddress;
@@ -9,15 +10,14 @@ typedef struct sockaddr_in socketAddress;
 void printError(const char* msg)
 {
     perror(msg);
-    // exit(0);
 }
 
 int main(int argc, char const *argv[])
 {
     //creation of client socket
-    int server_socket = 0;
+    int server_socket = -1;
     server_socket = socket(AF_INET,SOCK_STREAM,0);
-    if(server_socket < 0) printError("Server Socket not created\n");
+    if(server_socket < 0) printError("Server Socket not created");
     else printf("Welcome socket created successfully\n");
 
     socketAddress server_addr;
@@ -25,18 +25,36 @@ int main(int argc, char const *argv[])
     server_addr.sin_port = htons(S_PORT);
     server_addr.sin_addr.s_addr = INADDR_ANY;
     
-    int bind_status = 0;
+    int bind_status = -1;
     bind_status = bind(server_socket,(struct sockaddr* )&server_addr,sizeof(server_addr));
-    if(bind_status < 0) printError("Welcome socket bind failure\n");
+    if(bind_status < 0) printError("Welcome socket bind failure");
     else printf("Welcome socket bind success\n");
 
     listen(server_socket,5);
-    // int connection_status = connect(server_socket,(struct sockaddr* )&server_addr,sizeof(server_addr));
-    // if(connection_status < 0) printf("Connection error");
 
-    // char* request_msg = "UDP port requested";
-    // send(server_socket,request_msg,strlen(request_msg),0);
     int connection_socket;
-    if(connection_socket = accept(server_socket,(struct sockaddr* )&server_addr,(socklen_t* )sizeof(server_addr)) < 0) printf("Request not accepted\n");
+    socklen_t client_add_size;
+    socketAddress client_address;
+    if(connection_socket = accept(server_socket,(struct sockaddr* )&client_address,&client_add_size) < 0) printError("Request not accepted");
+    else printf("Connection Socket created successfully\n");
+
+    printf("Waiting for client request\n");
+
+    char client_request[1024] = {0};
+    int io_buffer = -1;
+    // if(io_buffer = recv(connection_socket,client_request,sizeof(client_request),0) < 0) printError("Error in recv");
+    if((io_buffer = read(connection_socket,client_request,1024)) < 0) printError("Error in recv");
+    else printf("Server received request : %s\n",client_request);
+
+    int UDP_PORT;
+    char UDP_PORT_str[1024];
+    printf("Please input port number\n");
+    scanf("%d",&UDP_PORT);
+
+    sprintf(UDP_PORT_str,"%d",UDP_PORT);
+    if(io_buffer = write(connection_socket,UDP_PORT_str,strlen(UDP_PORT_str)) < 0) printError("Error in send");
+    else printf("UDP_PORT sent by server\n");
+    
+    // close(connection_socket);
     return 0;
 }
